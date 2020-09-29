71703a1fef54287e9d313f6cff1a2a2985cf48fd
Tyler Butler <tylerbu@microsoft.com>
2020-09-29 15:12:58 -0700

    try eslint-tsdoc

--- packages/dds/cell/.eslintrc.js
+++ packages/dds/cell/.eslintrc.js
@@ -6,5 +6,11 @@
 module.exports = {
     "extends": [
         "@fluidframework/eslint-config-fluid/eslint7"
-    ]
+    ],
+    "plugins": [
+        "eslint-plugin-tsdoc",
+    ],
+    "rules": {
+        "tsdoc/syntax": "error",
+    }
 }
