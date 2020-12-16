9cbd2b3cf6332707d7b44b6605ec2b2195bd0d07
Tyler Butler <tylerbu@microsoft.com>
2020-12-16 10:55:31 -0800

    add lage

---
+++ lage.config.js
@@ -0,0 +1,53 @@
+module.exports = {
+    "pipeline": {
+        "l-build": [
+            "^build:copy",
+            "^tsc",
+            // "^build:compile",
+            "l-build:esnext",
+        ],
+        "l-build:esnext": [
+            "^l-build",
+        ],
+        "l-build:test": [
+            "l-build",
+            "^build:test",
+        ],
+        "l-lint": [
+            "eslint"
+        ],
+        "l-clean": [
+            "clean",
+        ],
+        "l-webpack": [
+            "l-build",
+            "^webpack",
+        ],
+
+        // Tasks below this should *only* have dependencies on lage tasks
+        "l-build:full": [
+            "l-build",
+            "l-lint",
+        ],
+        // "tsc": [
+        //     "^tsc"
+        // ],
+        "l-test": [
+            "l-build"
+        ],
+    },
+    "npmClient": "yarn",
+    "cacheOptions": {
+        // cacheStorageConfig: { provider: "local" },
+        // clearOutputFolder: false,
+        // internalCacheFolder: ".cache/backfill",
+        // logFolder: ".cache/backfill",
+        logLevel: "silly",
+        // mode: "READ_WRITE",
+        // name: "[name-of-package]",
+        outputGlob: ["lib/**", "dist/**"],
+        // packageRoot: ".",
+        // producePerformanceLogs: false,
+        // validateOutput: false
+    },
+};
