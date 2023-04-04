14c2b70e84c2e46e9dbef896aaf3c23a2540a2ec
Tyler Butler <tyler@tylerbutler.com>
2023-04-04 20:21:05 +0000

    wip

--- build-tools/packages/build-cli/src/lib/changesets.ts
+++ build-tools/packages/build-cli/src/lib/changesets.ts
@@ -3,14 +3,17 @@
  * Licensed under the MIT License.
  */
 
-import { VersionBumpType } from "@fluid-tools/version-tools";
+import { ReleaseVersion, VersionBumpType } from "@fluid-tools/version-tools";
 import { Logger, Package } from "@fluidframework/build-tools";
 import { existsSync, readFileSync, writeFileSync } from "fs";
 import matter from "gray-matter";
 
-// import type { Root } from "mdast";
-import type { Plugin } from "unified";
+// eslint-disable-next-line node/no-missing-import
+import type { Parent, Root, Paragraph, Heading, List, Text, Content } from "mdast";
+// eslint-disable-next-line node/no-missing-import
 import type { Node } from "unist";
+import type { Plugin } from "unified";
+
 import { Repository } from "./git";
 
 // IMPORTANT: TypeScript changes imports to require when outputting CJS, which causes dynamic import to fail. This hack
@@ -20,8 +23,16 @@ const dynamicImport = new Function("specifier", "return import(specifier)");
 
 let mdast;
 let mdastAssert;
+let mdastAssertParent;
 let mdastFromMarkdown;
+let mdastToMarkdown;
 let compact;
+let visit;
+let visitParents;
+
+let select;
+let matches;
+let selectAll;
 
 let root;
 let paragraph;
@@ -38,6 +49,7 @@ let remarkGitHub;
 let remarkNormalizeHeadings;
 let remarkParse;
 let remarkRemoveComments;
+let remarkStringify;
 let remarkToc;
 let unified;
 
@@ -47,7 +59,18 @@ let unified;
 	/* eslint-disable unicorn/no-await-expression-member */
 	// mdast = (await dynamicImport("mdast")).default;
 	compact = (await dynamicImport("mdast-util-compact")).compact;
-	mdastAssert = (await dynamicImport("mdast-util-assert")).assert;
+
+	const assertLib = await dynamicImport("mdast-util-assert");
+	mdastAssert = assertLib.assert;
+	mdastAssertParent = assertLib.parent;
+
+	visit = (await dynamicImport("unist-util-visit")).visit;
+	visitParents = (await dynamicImport("unist-util-visit-parents")).visitParents;
+
+	const unistSelect = await dynamicImport("unist-util-select");
+	select = unistSelect.select;
+	selectAll = unistSelect.selectAll;
+	matches = unistSelect.matches;
 
 	const builder = await dynamicImport("mdast-builder");
 	root = builder.root;
@@ -57,6 +80,7 @@ let unified;
 	brk = builder.brk;
 
 	mdastFromMarkdown = (await dynamicImport("mdast-util-from-markdown")).fromMarkdown;
+	mdastToMarkdown = (await dynamicImport("mdast-util-to-markdown")).toMarkdown;
 	remark = (await dynamicImport("remark")).remark;
 	remarkFrontmatter = (await dynamicImport("remark-frontmatter")).default;
 	remarkGfm = (await dynamicImport("remark-gfm")).default;
@@ -64,8 +88,9 @@ let unified;
 	remarkParse = (await dynamicImport("remark-parse")).default;
 	remarkRemoveComments = (await dynamicImport("remark-remove-comments")).default;
 	remarkNormalizeHeadings = (await dynamicImport("remark-normalize-headings")).default;
+	remarkStringify = (await dynamicImport("remark-stringify")).default;
 	remarkToc = (await dynamicImport("remark-toc")).default;
-	unified = await dynamicImport("unified");
+	unified = (await dynamicImport("unified")).unified;
 	/* eslint-enable unicorn/no-await-expression-member */
 })();
 
@@ -122,6 +147,17 @@ export async function getChangesetCommit(
 	return { summary: last.message, body: last.body, hash: last.hash };
 }
 
+const firstParagraphToHeading: Plugin = () => {
+	return (tree: Node) => {
+		// console.dir(tree);
+		mdastAssert(tree);
+		const paragraphs = selectAll("paragraph", tree);
+		const firstPara = paragraphs[0] as Heading;
+		firstPara.type = "heading";
+		firstPara.depth = 3;
+	};
+};
+
 export async function addChangesetsToChangelog(
 	changelogPath: string,
 	pkg: Package,
@@ -132,46 +168,82 @@ export async function addChangesetsToChangelog(
 		? readFileSync(changelogPath, "utf-8")
 		: `# ${pkg.name} Changelog\n\n`;
 
-	// const fileContent = remark()
-	// 	// .use(remarkFrontmatter, ["yaml"])
-	// 	.use(remarkGfm)
-	// 	.use(remarkGitHub)
-	// 	// .use(remarkRemoveComments)
-	// 	// eslint-disable-next-line unicorn/consistent-function-scoping
-	// 	.use((tree) => {
-	// 		log?.log(JSON.stringify(tree, null, 2));
-	// 	})
-	// 	.parse(changelog);
-
-	const changelogTree = mdastFromMarkdown(changelog);
-
+	const changelogEntries: Content[] = [];
 	for (const changeset of changesets) {
-		// const changesetContent = mdastFromMarkdown(changeset.rawContent);
 		// eslint-disable-next-line no-await-in-loop
-		const changesetContent = await remark()
-			.use(firstParagraphToHeading)
+		const changesetContent: Content = await unified()
+			.use(remarkParse)
 			.use(remarkGfm)
 			.use(remarkGitHub)
+			.use(firstParagraphToHeading)
+			// .use(buildChangelog(ver, changesets))
 			// .use(remarkNormalizeHeadings)
-			.process(changeset.rawContent);
-		// console.log(JSON.stringify(changesetContent, null, 2));
+			// .use(remarkStringify)
+			.parse(changeset.rawContent);
+		changelogEntries.push(changesetContent);
+		// console.warn(JSON.stringify(changesetContent, undefined, 2));
 	}
+
+	const fileContent = await unified()
+		.use(remarkParse)
+		.use(remarkGfm)
+		// .use(remarkGitHub)
+		.use(remarkRemoveComments)
+		// eslint-disable-next-line unicorn/consistent-function-scoping
+		.use(() => (tree) => {
+			// console.log(JSON.stringify(tree, undefined, 2));
+			visitParents(tree, (node: Node, ancestors: Node[]) => {
+				switch (node?.type) {
+					case "root": {
+						// console.dir((node as Root).children);
+						const children = (node as Root).children;
+						const topHeading = children[0];
+						mdastAssertParent(topHeading);
+
+						(node as Root).children = children.splice(1, 0, ...changelogEntries)
+						break;
+					}
+
+					case "text": {
+						// console.log(JSON.stringify(node, undefined, 2));
+						break;
+					}
+
+					case "heading": {
+						const h = node as Heading;
+						if (h.children[0].type === "text") {
+							const t: Text = h.children[0];
+							// if(t.value === )
+						}
+						break;
+					}
+
+					default: {
+						console.log(`skipped node type: ${node?.type}`);
+						break;
+					}
+				}
+
+				// console.log(`type: ${node?.type}, ${ancestors.length} ancestors`);
+			});
+		})
+		// .use(compact)
+		.use(remarkStringify)
+		.process(changelog);
+
+	// const fileContent = changelog + changesetStrings.join("\n\n\n");
+	// const processor = unified().use(remarkStringify, {
+	// 	bullet: "-",
+	// 	fence: "`",
+	// 	fences: true,
+	// 	incrementListMarker: false,
+	// });
+	// const fileContent = processor.stringify(changelogTree);
 
 	// console.log(JSON.stringify(tree, null, 2));
 
 	// log?.warning(String(fileContent));
 	// log?.warning(JSON.stringify(fileContent, null, 2));
 
-	// writeFileSync(changelogPath, fileContent);
+	writeFileSync(changelogPath, String(fileContent));
 }
-
-const firstParagraphToHeading: Plugin = () => {
-	return (tree) => {
-		console.log(tree);
-		mdastAssert(tree);
-		// const firstPara = tree.children[0];
-		// if (firstPara.type !== "paragraph") {
-		// 	return;
-		// }
-	};
-};
