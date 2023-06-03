f7517381100cce884b049d03c2a7385e53b3c353
Tyler Butler <tyler@tylerbutler.com>
2023-06-02 18:40:53 -0700

    changes

--- build-tools/packages/build-cli/src/commands/generate/changelog.ts
+++ build-tools/packages/build-cli/src/commands/generate/changelog.ts
@@ -7,13 +7,16 @@ import { Flags } from "@oclif/core";
 import { ensureFile } from "fs-extra";
 import matter from "gray-matter";
 import globby from "globby";
+import { strict as assert } from "node:assert";
 import path from "node:path";
 import fs from "node:fs/promises";
-import { format as prettier, resolveConfig as resolvePrettierConfig } from "prettier";
+import { format as prettier } from "prettier";
 import replace from "replace-in-file";
 
 import { PackageCommand } from "../../BasePackageCommand";
 import { ReleasePackage } from "../..";
+import { VersionBumpType } from "@fluid-tools/version-tools";
+import { CommandLogger } from "../../logging";
 
 export default class GenerateChangelogCommand extends PackageCommand<
 	typeof GenerateChangelogCommand
@@ -27,9 +30,6 @@ export default class GenerateChangelogCommand extends PackageCommand<
 		changed: Flags.boolean({
 			description:
 				"Generate changelogs only for packages with changesets that apply to them. Useful for testing.",
-			default: true,
-			allowNo: true,
-			helpGroup: "CHANGELOG GENERATION",
 		}),
 		...PackageCommand.flags,
 	};
@@ -42,6 +42,7 @@ export default class GenerateChangelogCommand extends PackageCommand<
 	];
 
 	private changesets: Map<ReleasePackage, string[]> = new Map();
+	private changeType: VersionBumpType | undefined;
 
 	public async init(): Promise<void> {
 		await super.init();
@@ -51,7 +52,12 @@ export default class GenerateChangelogCommand extends PackageCommand<
 		}
 
 		const context = await this.getContext();
-		this.changesets = await loadChangesets(path.join(context.repo.resolvedRoot, ".changeset"));
+		const { type, changesets } = await loadChangesets(
+			path.join(context.repo.resolvedRoot, ".changeset"),
+			this.logger,
+		);
+		this.changesets = changesets;
+		this.changeType = type;
 	}
 
 	protected async processPackage(directory: string): Promise<void> {
@@ -64,9 +70,9 @@ export default class GenerateChangelogCommand extends PackageCommand<
 			this.warning(`Package ${pkgName} not found in context.`);
 		}
 
-		const entries = this.changesets.get(pkgName);
 		const pattern = new RegExp(`# ${pkgName}\n\n`);
 		let replacement = `# ${pkgName}\n\n## ${version}\n\n`;
+		const entries = this.changesets.get(pkgName);
 
 		if (entries === undefined || entries.length === 0) {
 			this.verbose(`${pkgName}: No changes`);
@@ -78,6 +84,10 @@ export default class GenerateChangelogCommand extends PackageCommand<
 			this.warning(
 				`${pkgName}: ${entries.length} ${entries.length === 1 ? "change" : "changes"}`,
 			);
+			replacement += `### ${
+				// eslint-disable-next-line unicorn/prefer-spread
+				this.changeType?.charAt(0)?.toUpperCase().concat(this.changeType?.slice(1))
+			} Changes\n\n`;
 			for (const entry of entries) {
 				replacement += `-   ${entry.trim()}\n\n`;
 			}
@@ -95,21 +105,36 @@ export default class GenerateChangelogCommand extends PackageCommand<
 	}
 }
 
-async function loadChangesets(dir: string): Promise<Map<ReleasePackage, string[]>> {
+async function loadChangesets(
+	dir: string,
+	log?: CommandLogger,
+): Promise<{
+	type: VersionBumpType;
+	changesets: Map<ReleasePackage, string[]>;
+}> {
 	const changesetMap = new Map<ReleasePackage, string[]>();
 	const changesetFiles = await globby(["*.md", "!README.md"], { cwd: dir, absolute: true });
+	let changeType: VersionBumpType | undefined;
 
 	for (const file of changesetFiles) {
 		const m = matter.read(file);
 
-		for (const [pkgName] of Object.entries(m.data)) {
+		for (const [pkgName, type] of Object.entries(m.data)) {
+			changeType = changeType ?? type;
+			if (type !== changeType) {
+				log?.warning(`Unexpected change type: ${type} (expected ${changeType})`);
+			}
 			const entries = changesetMap.get(pkgName) ?? [];
 			entries.push(m.content);
 			changesetMap.set(pkgName, entries);
 		}
 	}
 
-	return changesetMap;
+	if (changeType === undefined) {
+		throw new Error(`No change type found.`);
+	}
+
+	return { type: changeType, changesets: changesetMap };
 }
 
 async function prettierFile(file: string): Promise<void> {
