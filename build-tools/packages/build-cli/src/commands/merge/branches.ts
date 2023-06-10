0ede2a484800c7064f7556eeead8f1feabe8759f
Tyler Butler <tyler@tylerbutler.com>
2023-06-09 17:56:27 -0700

    wip

--- build-tools/packages/build-cli/src/commands/merge/branches.ts
+++ build-tools/packages/build-cli/src/commands/merge/branches.ts
@@ -2,11 +2,18 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
+import { GitRepo, Logger } from "@fluidframework/build-tools";
 import { Flags } from "@oclif/core";
+import chalk from "chalk";
 
 import { BaseCommand } from "../../base";
-import { createPullRequest, getUserAccess, pullRequestExists, pullRequestInfo } from "../../lib";
-import { GitRepo } from "@fluidframework/build-tools";
+import {
+	Repository,
+	createPullRequest,
+	getUserAccess,
+	pullRequestExists,
+	pullRequestInfo,
+} from "../../lib";
 
 interface CommitStatus {
 	isConflict: boolean;
@@ -23,9 +30,10 @@ export default class MergeBranch extends BaseCommand<typeof MergeBranch> {
 
 	static flags = {
 		auth: Flags.string({
-			description: "GitHub authentication token",
+			description: "GitHub authentication token.",
 			char: "a",
 			required: true,
+			env: "GITHUB_TOKEN",
 		}),
 		source: Flags.string({
 			description: "Source branch name",
@@ -46,37 +54,29 @@ export default class MergeBranch extends BaseCommand<typeof MergeBranch> {
 	};
 
 	/**
-	 *
-	 * Function always returns an index in the array. The Boolean indicates whether the commit at that index conflicts or not.
-	 * If the Boolean is false, then you know that all commits in the list are mergable.
-	 * If the Boolean is true, then it indicates that the indexed commit conflicts.
-	 * The primary role of the function is to check a list of commits for conflicts, and if there is one, to return the index of the conflict.
-	 * hasConflicts(commits): [boolean, index]
+	 * The branch that the command was run from. This is used to checkout the branch in the case of a failure.
 	 */
-	async hasConflicts(commitIds: string[], gitRepo: GitRepo): Promise<CommitStatus> {
-		const length = commitIds.length;
-		for (let i = 0; i < length; i++) {
-			const commit = commitIds[i];
-			// eslint-disable-next-line no-await-in-loop
-			const mergesClean = await gitRepo.canMergeWithoutConflicts(commit);
-			this.log(`Response from merge check for ${commitIds[i]}: ${mergesClean}`);
-			if (mergesClean === false) {
-				return { isConflict: true, index: i };
-			}
-		}
-
-		// No conflicts found, return the last index
-		return { isConflict: false, index: length - 1 };
-	}
+	private initialBranch: string = "";
+	/**
+	 * A list of branches that should be deleted if the command fails.
+	 */
+	private readonly branchesToCleanup: string[] = [];
 
 	public async run(): Promise<void> {
 		const flags = this.flags;
 
-		const title: string = `Automation: ${flags.source}-${flags.target} integrate`;
+		const prTitle: string = `Automation: ${flags.source}-${flags.target} integrate`;
 
 		const context = await this.getContext();
-		const gitRepo = context.gitRepo;
-		const prExists: boolean = await pullRequestExists(flags.auth, title, this.logger);
+		const gitRepo = new Repository({ baseDir: context.gitRepo.resolvedRoot });
+		// eslint-disable-next-line unicorn/no-await-expression-member
+		this.initialBranch = (await gitRepo.gitClient.status()).current ?? "main";
+
+		// Get the name of the remote that corresponds to the Microsoft fluid repo
+		// const remote = gitRepo.getRemote(context.originRemotePartialUrl);
+		const remote = "origin";
+
+		const prExists: boolean = await pullRequestExists(flags.auth, prTitle, this.logger);
 
 		if (prExists) {
 			this.exit(-1);
@@ -84,132 +84,213 @@ export default class MergeBranch extends BaseCommand<typeof MergeBranch> {
 			// TODO: notify the author
 		}
 
-		const lastMergedCommit = await gitRepo.mergeBase(flags.source, flags.target);
+		const lastMergedCommit = await gitRepo.getMergeBase(flags.source, flags.target);
 		this.log(
 			`${lastMergedCommit} is the last merged commit id between ${flags.source} and ${flags.target}`,
 		);
 
-		const unmergedCommitList: string[] = [];
-		const revListOutput = await gitRepo.revList(lastMergedCommit, flags.source);
-		const commitLines = revListOutput.split("\n");
+		const unmergedCommitList: string[] = await gitRepo.revList(
+			lastMergedCommit,
+			`refs/remotes/${remote}/${flags.source}`,
+		);
 
-		for (const line of commitLines) {
-			const id = line.trim();
-			unmergedCommitList.push(id);
+		if (unmergedCommitList.length === 0) {
+			this.log(
+				chalk.green(
+					`${flags.source} and ${flags.target} branches are in sync. No commits to merge`,
+				),
+			);
+			this.exit(-1);
 		}
 
 		this.log(
 			`There are ${unmergedCommitList.length} unmerged commits between ${flags.source} and ${flags.target} branches`,
 		);
 
-		if (unmergedCommitList.length === 0) {
-			this.log(
-				`${flags.source} and ${flags.target} branches are in sync. No commits to merge`,
-			);
-			this.exit(-1);
-		}
-
 		const commitSize = Math.min(flags.batchSize, unmergedCommitList.length);
 		// `branchToCheckConflicts` is used to check the conflicts of each commit with next.
-		const branchToCheckConflicts = `${flags.target}-automation`;
+		const tempBranchToCheckConflicts = `${flags.target}-automation`;
+		this.branchesToCleanup.push(tempBranchToCheckConflicts);
 
-		await gitRepo.switchBranch(flags.target);
-		await gitRepo.createBranch(branchToCheckConflicts);
-		await gitRepo.setUpstream(branchToCheckConflicts);
+		await gitRepo.gitClient.checkoutBranch(tempBranchToCheckConflicts, flags.target);
+		// await gitRepo.gitClient.branch([
+		// 	"--set-upstream-to",
+		// 	`${remote}/${tempBranchToCheckConflicts}`,
+		// ]);
 
-		const commitInfo = await this.hasConflicts(
+		const [commitListHasConflicts, conflictingCommitIndex] = await hasConflicts(
 			unmergedCommitList.slice(0, commitSize),
 			gitRepo,
+			this.logger,
 		);
-
-		let commitId: string;
+		this.verbose(`conflicting commit: ${conflictingCommitIndex}`);
 
 		/**
-		 * `commitInfo.isConflict === true && commitInfo.index === 0` implies the first index has conflicts with next and open a single PR
-		 * `commitInfo.isConflict === true && commitInfo.index !== 0` imples open PR till the last non-conflicting commit `--commitInfo.index` and set `isConflict` to false so that next can be merged later on
+		 * The commit that should be used as the HEAD for the PR.
 		 */
-		if (commitInfo.isConflict === true && commitInfo.index === 0) {
-			commitId = unmergedCommitList[0];
-		} else if (commitInfo.isConflict === true && commitInfo.index !== 0) {
-			commitId = unmergedCommitList[--commitInfo.index];
-			commitInfo.isConflict = false;
+		let prHeadCommit = "";
+
+		/**
+		 * Defaults to true but will be set to false if the PR is expected to not have conflicts.
+		 */
+		let prWillConflict: boolean;
+
+		if (commitListHasConflicts === true) {
+			// There's a conflicting commit in the list, so we need to determine which commit to use as the HEAD for the PR.
+			if (conflictingCommitIndex === 0) {
+				// If it's the first item in the list that conflicted, then we want to open a single PR with the HEAD at that
+				// commit. The PR is expected to conflict with the target branch, so we leave prWillConflict to truethe PR owner will need to merge the branch
+				// manually with next and push back to the PR, and then merge once CI passes.
+				prHeadCommit = unmergedCommitList[0];
+				prWillConflict = true;
+			} else {
+				// Otherwise, we want to open a PR with the HEAD at the commit BEFORE the first conflicting commit.
+				prHeadCommit = unmergedCommitList[conflictingCommitIndex - 1];
+				prWillConflict = false;
+			}
 		} else {
-			commitId = unmergedCommitList[commitInfo.index];
+			// No conflicting commits, so set the commit to merge to the last commit in the list.
+			prHeadCommit = unmergedCommitList[unmergedCommitList.length - 1];
+			prWillConflict = false;
 		}
 
-		const branchName = `${flags.source}-${flags.target}-${commitId.slice(0, 7)}`;
+		const branchName = `${flags.source}-${flags.target}-${shortCommit(prHeadCommit)}`;
+		this.branchesToCleanup.push(branchName);
 
-		await gitRepo.deleteBranch(branchToCheckConflicts);
-		await gitRepo.switchBranch(flags.source);
-		await gitRepo.createBranch(branchName);
-		await gitRepo.setUpstream(branchName);
-		await gitRepo.resetBranch(commitId);
+		this.verbose(`Creating and checking out branch: ${branchName} at commit ${prHeadCommit}`);
+		await gitRepo.gitClient
+			.checkoutBranch(branchName, prHeadCommit)
+			.push(remote, branchName)
+			.branch(["--set-upstream-to", `${remote}/${branchName}`]);
+
+		this.verbose(`Deleting temp branch: ${tempBranchToCheckConflicts}`);
+		await gitRepo.gitClient.branch(["-D", tempBranchToCheckConflicts]);
+
+		// this.verbose(`Setting upstream to ${remote} for branch: ${branchName}`);
+		// await gitRepo.gitClient.setUpstream(branchName, remote);
 
 		/**
 		 * The below description is intended for PRs which has merge conflicts with next.
 		 */
 		let description: string = `## ${flags.source}-${flags.target} integrate PR
 		The aim of this pull request is to sync ${flags.source} and ${flags.target} branch. This commit has **MERGE CONFLICTS** with ${flags.target}. The expectation from the assignee is as follows:
-					
+
 		> - Acknowledge the pull request by adding a comment -- "Actively working on it".
-					
+
 		> - Merge ${flags.target} into this ${branchName}.
-					
+
 		> - Resolve any merge conflicts between ${branchName} and ${flags.target} and push the resolution to this branch: ${branchName}. **Do NOT rebase or squash this branch: its history must be preserved**.
-					
+
 		> - Ensure CI is passing for this PR, fixing any issues.
-					
-		> - Recommended git commands: 
+
+		> - Recommended git commands:
 		git checkout ${branchName}
 		git merge ${flags.target}
 		**RESOLVE MERGE CONFLICTS**
 		git add .
-		git commit -m ${title}
+		git commit -m ${prTitle}
 		git push`;
 
-		if (commitInfo.isConflict === false) {
-			await gitRepo.mergeBranch(flags.target, title);
+		if (prWillConflict === false) {
+			await gitRepo.gitClient.merge([flags.target, "-m", prTitle]);
 
 			/**
 			 * The below description is intended for PRs which may have CI failures with next.
 			 */
 			description = `## ${flags.source}-${flags.target} integrate PR
 			The aim of this pull request is to sync ${flags.source} and ${flags.target} branch. The expectation from the assignee is as follows:
-						
+
 			> - Acknowledge the pull request by adding a comment -- "Actively working on it".
-						
+
 			> - Resolve any CI failures between ${branchName} and ${flags.target} thereby pushing the resolution to this branch: ${branchName}. **Do NOT rebase or squash this branch: its history must be preserved**.
-						
+
 			> - Ensure CI is passing for this PR, fixing any issues. Please don't look into resolving **Real service e2e test** and **Stress test** failures as they are **non-required** CI failures.
-			
-			> - Recommended git commands: 
+
+			> - Recommended git commands:
 			git checkout ${branchName}
 			**FIX THE CI FAILURES**
-			git commit --amend -m ${title}
+			git commit --amend -m ${prTitle}
 			git push --force-with-lease`;
 		}
 
 		/**
 		 * fetch name of owner associated to the pull request
 		 */
-		const pr = await pullRequestInfo(flags.auth, commitId, this.logger);
-		const author = pr.data[0].assignee.login;
-		this.info(`Fetch pull request info for commit id ${commitInfo} and assignee ${author}`);
+		const pr = await pullRequestInfo(flags.auth, prHeadCommit, this.logger);
+		console.debug(pr);
+		const author = pr.data?.[0]?.assignee?.login;
+		this.info(
+			`Fetching pull request info for commit id ${prHeadCommit} and assignee ${author}`,
+		);
 		const user = await getUserAccess(flags.auth, this.logger);
-		this.info(`List users with push access to main branch ${user}`);
+		this.info(`List users with push access to main branch: ${user}`);
 
 		const prObject = {
 			token: flags.auth,
 			source: branchName,
 			target: flags.target,
 			assignee: author,
-			title,
+			title: prTitle,
 			description,
 		};
 
 		const prNumber = await createPullRequest(prObject, this.logger);
 		this.log(
-			`Opened pull request ${prNumber} for commit id ${commitId}. Please resolve the merge conflicts.`,
+			`Opened pull request ${prNumber} for commit id ${prHeadCommit}. Please resolve the merge conflicts.`,
 		);
 	}
+
+	protected override async catch(err: Error & { exitCode?: number | undefined }): Promise<any> {
+		const context = await this.getContext();
+		const gitRepo = context.gitRepo;
+
+		// Check out the initial branch
+		this.warning(`CLEANUP: checking out initial branch ${this.initialBranch}`);
+		await gitRepo.switchBranch(this.initialBranch);
+
+		// Delete the branches we created
+		const promises: Promise<void>[] = [];
+		for (const branch of this.branchesToCleanup) {
+			this.warning(`CLEANUP: Deleting branch ${branch}`);
+			promises.push(gitRepo.deleteBranch(branch));
+		}
+		await Promise.all(promises);
+		throw err;
+	}
+}
+
+/**
+ */
+
+/**
+ * Function always returns an index in the array. The Boolean indicates whether the commit at that index conflicts or
+ * not. If the Boolean is false, then you know that all commits in the list are mergable. If the Boolean is true, then
+ * it indicates that the indexed commit conflicts. The primary role of the function is to check a list of commits for
+ * conflicts, and if there is one, to return the index of the conflict.
+ *
+ * @param commitIds - An array of commit ids to check for conflicts.
+ * @param gitRepo - The git repository to check for conflicts in.
+ * @param log - An optional logger to use.
+ * @returns a Boolean that indicates whether the commit at that index conflicts or not
+ */
+export async function hasConflicts(
+	commitIds: string[],
+	gitRepo: Repository,
+	log?: Logger,
+): Promise<[boolean, number]> {
+	for (const [i, commit] of commitIds.entries()) {
+		// eslint-disable-next-line no-await-in-loop
+		const mergesClean = await gitRepo.canMergeWithoutConflicts(commit);
+		log?.verbose(`Can merge without conflicts ${commit}: ${mergesClean}`);
+		if (mergesClean === false) {
+			return [true, i];
+		}
+	}
+
+	// No conflicts found, return the last index
+	return [false, commitIds.length - 1];
+}
+
+function shortCommit(commit: string): string {
+	return commit.slice(0, 7);
 }
