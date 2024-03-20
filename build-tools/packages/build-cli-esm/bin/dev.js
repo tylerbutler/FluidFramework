1c585476f0c6a1fe9eca818fcbe20a6b7713f0f9
Tyler Butler <tylerbu@microsoft.com>
2024-03-19 18:25:30 -0700

    new project

--- build-tools/packages/build-cli-esm/bin/dev.js
+++ build-tools/packages/build-cli-esm/bin/dev.js
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 #!/usr/bin/env -S node --loader ts-node/esm --no-warnings=ExperimentalWarning
 
 import { execute } from "@oclif/core";
