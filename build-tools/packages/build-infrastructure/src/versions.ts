1393120965fc61164cdca0e508711b01dac0a9ea
Tyler Butler <tylerbu@microsoft.com>
2024-10-14 15:12:17 -0700

    wip

--- build-tools/packages/build-infrastructure/src/versions.ts
+++ build-tools/packages/build-infrastructure/src/versions.ts
@@ -6,24 +6,23 @@
 import * as semver from "semver";
 
 import { updatePackageJsonFile } from "./packageJsonUtils.js";
-import type { IFluidRepo, PackageJson } from "./types.js";
+import type { IFluidRepo, IPackage, PackageJson } from "./types.js";
 
 /**
- * Sets the version of a release group or standalone package.
+ * Sets the version of a group of packages.
  *
  * @param fluidRepo - The {@link IFluidRepo}.
  * @param items - An array of objects whose version should be updated.
  * @param version - The version to set.
- * @param log - A logger to use.
  */
-export async function setVersion(
+export async function setVersion<J extends PackageJson>(
 	fluidRepo: IFluidRepo,
-	items: { directory: string }[],
+	items: IPackage[],
 	version: semver.SemVer,
 ): Promise<void> {
 	const translatedVersion = version;
 	for (const pkg of items) {
-		updatePackageJsonFile(pkg.directory, (json: PackageJson) => {
+		updatePackageJsonFile<J>(pkg.directory, (json) => {
 			json.version = translatedVersion.version;
 		});
 	}
