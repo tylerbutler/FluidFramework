7a2fc6c799e418fc9e33b8a697b8835deb267c7e
Tyler Butler <tylerbu@microsoft.com>
2024-10-15 17:37:53 -0700

    oclif command

--- build-tools/packages/build-infrastructure/src/commands/list.ts
+++ build-tools/packages/build-infrastructure/src/commands/list.ts
@@ -3,52 +3,86 @@
  * Licensed under the MIT License.
  */
 
-import { program } from "@commander-js/extra-typings";
+import { Command, Flags } from "@oclif/core";
 import colors from "picocolors";
 
 import { getAllDependenciesInRepo, loadFluidRepo } from "../fluidRepo.js";
+import type { IFluidRepo } from "../types.js";
 
-program
-	// .command("list")
-	// .argument("[file]")
-	.option("--cwd <searchPath>", "Path to a Fluid repo", process.cwd())
-	.action(({ cwd }) => {
+export class ListCommand extends Command {
+	static override description =
+		"List objects in the Fluid repo, like release groups, workspaces, and packages.";
+
+	static override flags = {
+		path: Flags.directory({
+			description: "Path to start searching for the Fluid repo.",
+			default: ".",
+		}),
+		full: Flags.boolean({
+			description: "Output the full report.",
+		}),
+	} as const;
+
+	async run(): Promise<void> {
+		const { flags } = await this.parse(ListCommand);
+		const { path: searchPath, full } = flags;
+
 		// load the fluid repo
-		const repo = loadFluidRepo(cwd);
+		const repo = loadFluidRepo(searchPath);
+		const _ = full ? await this.logFullReport(repo) : await this.logCompactReport(repo);
+	}
 
-		logIndent(colors.underline("Repository layout"));
+	private async logFullReport(repo: IFluidRepo): Promise<void> {
+		this.logIndent(colors.underline("Repository layout"));
 		for (const workspace of repo.workspaces.values()) {
-			logIndent(colors.blue(workspace.toString()), 1);
+			this.log();
+			this.logIndent(colors.blue(workspace.toString()), 1);
 			for (const releaseGroup of workspace.releaseGroups.values()) {
-				logIndent(colors.green(releaseGroup.toString()), 2);
-				// console.log();
-				logIndent("Packages", 3);
+				this.log();
+				this.logIndent(colors.green(releaseGroup.toString()), 2);
+				this.logIndent(colors.bold("Packages"), 3);
 				for (const pkg of releaseGroup.packages) {
 					const pkgMessage = colors.white(
 						`${pkg.name}${pkg.isReleaseGroupRoot ? colors.bold(" (root)") : ""}`,
 					);
-					logIndent(pkgMessage, 4);
+					this.logIndent(pkgMessage, 4);
 				}
 
 				const { releaseGroups, workspaces } = getAllDependenciesInRepo(
 					repo,
 					releaseGroup.packages,
 				);
-				console.log();
-				logIndent("Depends on:", 3);
-				for (const depReleaseGroup of releaseGroups) {
-					logIndent(depReleaseGroup.toString(), 4);
-				}
-				for (const depWorkspace of workspaces) {
-					logIndent(depWorkspace.toString(), 4);
+				if (releaseGroups.length > 0 || workspaces.length > 0) {
+					this.log();
+					this.logIndent(colors.bold("Depends on:"), 3);
+					for (const depReleaseGroup of releaseGroups) {
+						this.logIndent(depReleaseGroup.toString(), 4);
+					}
+					for (const depWorkspace of workspaces) {
+						this.logIndent(depWorkspace.toString(), 4);
+					}
 				}
 			}
 		}
-	});
+	}
 
-program.parse();
+	private async logCompactReport(repo: IFluidRepo): Promise<void> {
+		this.logIndent(colors.underline("Repository layout"));
+		for (const workspace of repo.workspaces.values()) {
+			this.log();
+			this.logIndent(colors.blue(workspace.toString()), 1);
+			this.logIndent(colors.bold("Packages"), 2);
+			for (const pkg of workspace.packages) {
+				const pkgMessage = colors.white(
+					`${pkg.isReleaseGroupRoot ? colors.bold("(root) ") : ""}${pkg.name} ${colors.black(colors.bgGreen(pkg.releaseGroup))}`,
+				);
+				this.logIndent(pkgMessage, 3);
+			}
+		}
+	}
 
-function logIndent(message: string, indent: number = 0): void {
-	const spaces = " ".repeat(2 * indent);
-	console.log(`${spaces}${message}`);
+	private logIndent(message: string, indent: number = 0): void {
+		const spaces = " ".repeat(2 * indent);
+		this.log(`${spaces}${message}`);
+	}
 }
