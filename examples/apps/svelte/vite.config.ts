e33e36835dabeda932b07d45d275361b3eef030c
Tyler Butler <tylerbu@microsoft.com>
2025-02-26 10:44:44 -0800

    install tailwindcss

--- examples/apps/svelte/vite.config.ts
+++ examples/apps/svelte/vite.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from "@tailwindcss/vite";
 /*!
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
@@ -7,7 +8,7 @@ import { defineConfig } from "vitest/config";
 import { sveltekit } from "@sveltejs/kit/vite";
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), tailwindcss()],
 
 	test: {
 		include: ["src/**/*.{test,spec}.{js,ts}"],
