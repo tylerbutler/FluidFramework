03085cc54f5e081dee053ffaca7b7b79c9069e7e
Tyler Butler <tylerbu@microsoft.com>
2025-02-27 13:03:19 -0800

    cleanup

--- examples/apps/svelte/src/routes/s/[containerId]/+page.svelte
+++ examples/apps/svelte/src/routes/s/[containerId]/+page.svelte
@@ -3,8 +3,6 @@ import type { PageProps } from "./$types";
 import { Alert } from 'flowbite-svelte';
 import Sudoku from "../../../Sudoku.svelte";
 import { loadIncludedPuzzle } from "../../../sudokuPuzzle.svelte";
-import { Alert } from 'flowbite-svelte';
-import { Alert as A2 } from "svelte-5-ui-lib";
 
 const { data }: PageProps = $props();
 const { presence, sessionClient } = data;
@@ -19,13 +17,6 @@ let puzzle = loadIncludedPuzzle(0);
     <span class="font-medium">Testing!</span>
     Is it working?
   </Alert>
-</div>
-
-<div class="p-8">
-  <A2>
-    <span class="font-medium">Testing!</span>
-    Is it working?
-	</A2>
 </div>
 
 <Sudoku {puzzle} {presence} {sessionClient} />
